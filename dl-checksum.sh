92e0e24ae111fb378c123b51b75eb872722df8d3
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-10-19 21:12:06 -0400

    faascli:0.14.11

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -36,27 +36,4 @@ dl_ver() {
     dl $ver windows amd64
 }
 
-dl_ver 0.9.2
-dl_ver 0.11.7
-dl_ver 0.11.8
-dl_ver 0.12.3
-dl_ver 0.12.4
-dl_ver 0.12.5
-dl_ver 0.12.6
-dl_ver 0.12.7
-dl_ver 0.12.8
-dl_ver 0.12.9
-dl_ver 0.12.13
-dl_ver 0.12.14
-dl_ver 0.12.19
-dl_ver 0.12.20
-dl_ver 0.12.22
-dl_ver 0.13.0
-dl_ver 0.13.9
-dl_ver 0.13.10
-dl_ver 0.13.15
-dl_ver 0.14.1
-dl_ver 0.14.2
-dl_ver 0.14.5
-dl_ver 0.14.6
-dl_ver ${1:-0.14.7}
+dl_ver ${1:-0.14.11}
